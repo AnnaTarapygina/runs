7b5a19fdc2807210bb3d4d075860cccd3cddf5ee
AnnaTarapygina <53184041+AnnaTarapygina@users.noreply.github.com>
2021-09-14 08:08:16 -0500

    Update main.tf

--- local_wait/main.tf
+++ local_wait/main.tf
@@ -17,15 +17,15 @@ resource "random_integer" "timeout" {
 }
 
 resource "null_resource" "wait" {
-  triggers = {
-    run_id = var.run_id
-  }
   provisioner "local-exec" {
-    command = "env"
+    command = <<EOT
+      sleep 120
+    EOT
+  }
+  triggers = {
+    always_run = timestamp()
   }
 }
-
-
 
 output "very_long" {
   value = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"  
