8f04b38ab9c0f23850b208074d8093176e74614b
AnnaTarapygina <53184041+AnnaTarapygina@users.noreply.github.com>
2021-11-24 14:19:53 +0200

    Update main.tf

--- local_wait/main.tf
+++ local_wait/main.tf
@@ -19,7 +19,7 @@ resource "random_integer" "timeout" {
 resource "null_resource" "wait" {
   provisioner "local-exec" {
     command = <<EOT
-      sleep 360
+      sleep 160
     EOT
   }
   triggers = {
